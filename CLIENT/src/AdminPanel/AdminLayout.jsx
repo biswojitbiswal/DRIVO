1260c8de6f5cb949fecbdc4a1d78a0b26532e617
Biswojit Biswal <biswojitb474@gmail.com>
2024-10-25 15:45:05 +0530

    adjust admin nav menu

--- CLIENT/src/AdminPanel/AdminLayout.jsx
+++ CLIENT/src/AdminPanel/AdminLayout.jsx
@@ -2,40 +2,35 @@ import React, { useState } from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 
 function AdminLayout() {
-    const [menu, setMenu] = useState('dashboard')
-
-    const handleNavbar = (menuItem) => {
-        setMenu(menuItem);
-    }
 
   return (
     <>
       <section>
         <div className="admin-controller">
-            <div className="admin-nav">
+          <div className="admin-nav">
             <ul>
-                    <li onClick={() => handleNavbar('dashboard')} className={menu === 'dashboard' ? 'admin-nav-active' : ''}>
-                        <NavLink to="/admin">Dashboard</NavLink>
-                    </li>
-                    
-                    <li onClick={() => handleNavbar('users')} className={menu === 'users' ? 'admin-nav-active' : ''}>
-                        <NavLink to="/admin/users">Users</NavLink>
-                    </li>
-                    <li onClick={() => handleNavbar('contacts')} className={menu === 'contacts' ? 'admin-nav-active' : ''}>
-                        <NavLink to="/admin/contacts">Contacts</NavLink>
-                    </li>
-                    <li onClick={() => handleNavbar('vehicle')} className={menu === 'vehicle' ? 'admin-nav-active' : ''}>
-                        <NavLink to="/admin/vehicles">Vehicle</NavLink>
-                    </li>
-                    
-                </ul>
-            
-            </div>
-            <Outlet />
-            
+              <li>
+                <NavLink to="/admin" className={({ isActive }) => (isActive ? 'admin-nav-active' : "")} end>Dashboard</NavLink>
+                {/* end prop. This ensures that the active class is only applied when the path matches exactly. */}
+              </li>
+
+              <li>
+                <NavLink to="/admin/users" className={({ isActive }) => (isActive ? 'admin-nav-active' : "")}>Users</NavLink>
+              </li>
+              <li>
+                <NavLink to="/admin/contacts" className={({ isActive }) => (isActive ? 'admin-nav-active' : "")}>Contacts</NavLink>
+              </li>
+              <li>
+                <NavLink to="/admin/vehicles" className={({ isActive }) => (isActive ? 'admin-nav-active' : "")}>Vehicle</NavLink>
+              </li>
+
+            </ul>
+          </div>
+          <Outlet />
+
         </div>
       </section>
-      
+
     </>
   )
 }
