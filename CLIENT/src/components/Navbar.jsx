a48fb214555e0e82d1e2f1092d10464f10a6d482
Biswojit Biswal <biswojitb474@gmail.com>
2024-10-28 01:07:53 +0530

    some correction

--- CLIENT/src/components/Navbar.jsx
+++ CLIENT/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 import { useAuth } from '../Store/Auth';
 
 
